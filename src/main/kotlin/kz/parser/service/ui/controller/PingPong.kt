20ecc7a0b1ecbd362c26e811da5c6301adee4b8a
Baurr <bramatdinov776@gmail.com>
2024-04-25 15:55:48 +0500

    proxy and conf

---
+++ src/main/kotlin/kz/parser/service/ui/controller/PingPong.kt
@@ -0,0 +1,19 @@
+package kz.parser.service.ui.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/ping")
+@CrossOrigin("*")
+class PingPong {
+
+    @GetMapping("")
+    fun pong(): ResponseEntity<Any> {
+        return ResponseEntity.ok("pong")
+    }
+
+}
