1ad9a3d7d14b8154996abef73bee391d02ec6234
Baurr <bramatdinov776@gmail.com>
2024-04-25 15:57:56 +0500

    pref api

--- src/main/kotlin/kz/parser/service/ui/controller/PingPong.kt
+++ src/main/kotlin/kz/parser/service/ui/controller/PingPong.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/ping")
+@RequestMapping("/ping")
 @CrossOrigin("*")
 class PingPong {
 
